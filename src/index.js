7e69aefd8884d6866db03d62a0caec4ec754e7b1
arwnchrstn <ceres703@gmail.com>
2022-08-20 20:15:29 +0800

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
-import App from "./App";
 import "aos/dist/aos.css";
 import Aos from "aos";
 Aos.init({ duration: 600, easing: "ease-out", once: true });
+const App = React.lazy(() => import("./App"));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const loader = (
+  <div
+    className="position-fixed w-100 h-100 d-flex justify-content-center align-items-center bg-light"
+    style={{ zIndex: 9999 }}
+  >
+    <div
+      class="spinner-grow text-primary "
+      style={{ width: "6rem", height: "6rem" }}
+      role="status"
+    >
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <Suspense fallback={loader}>
+      <App />
+    </Suspense>
+  </>
 );
